510893429640ac7704b0bb47a4a8c983035be360
EV <vzhenia@users.noreply.github.com>
2023-06-11 12:46:22 +0200

    added pwd error, clear button

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ const errorMessages = {
 
 /* Function that creates error message if field doesn't pass validation */
 const getNameError = (name) => {
+    // Sanity check
     if (typeof(name) !== "string") {
         return errorMessages.somethingWrong;
     }
+    // Empty field validation
     if (!name.length) {
         return errorMessages.required;
     }
+    // Validate name length according to business logic
     if (name.length < 3) {
         return errorMessages.invalidNameLength;
     }
+    // If there are no errors, return empty string
     return "";
 }
 
@@ -43,49 +47,69 @@ const getCountries = async () => {
 
 async function createCountiesOptions(){
     const countries = await getCountries();
-    const selectElement = document.getElementById("countries")
+    const selectElement = document.getElementById("countries");
     
     for (const country of countries.data) {
-        const option = document.createElement('option')
-        option.setAttribute("id", country.code)
-        const cName = document.createTextNode(country.name)
-        option.appendChild(cName)
-        selectElement.appendChild(option)
+        const option = document.createElement('option');
+        option.setAttribute("id", country.code);
+        const cName = document.createTextNode(country.name);
+        option.appendChild(cName);
+        selectElement.appendChild(option);
     }
     console.log('Dropdown with countries built...')
 }
 
 (async function() {
     await createCountiesOptions();
-    console.log('Now we continue....')
+    console.log('Now we can collect user input....')
+
     const name = document.getElementById('name');
     const password = document.getElementById('password');
+    const country = document.getElementById('countries');
     const submitButton = document.getElementById('submit');
-    
+    const nameErrorDisplay = document.getElementById("nameErrorDisplay");
+    const passwordErrorDisplay = document.getElementById("passwordErrorDisplay");
+    const clearButton = document.getElementById("clear_Form");
+
     let nameTxt = "";
     let passwordTxt = "";
+    let countryTxt = country.value;
 
     // This shows how you can access event.target.value
     name.oninput = function(e) {
         nameTxt = e.target.value;
+        nameErrorDisplay.textContent = "";
         console.log('Name: event.target.value=', e.target.value)
     }
     password.oninput = function(e) { 
         passwordTxt = e.target.value;
+        passwordErrorDisplay.textContent = "";
         console.log('Password: event.target.value=', e.target.value)
+    }
+    country.onchange = function(e) {
+        countryTxt = e.target.value;
+        console.log('Selected country: event.target.value=', e.target.value)
+    }
+
+    clearButton.onclick = function(e) {
+        nameErrorDisplay.textContent = "";
+        passwordErrorDisplay.textContent = "";
     }
 
     // Here we assign a function to "onclick" method of Submit button
-    submitButton.onclick = function validator(e) {
+    submitButton.onclick = function(e) {
         e.preventDefault();
         console.log("Submit button is clicked....")
-        console.log('Submitted Name=', nameTxt)
+        console.log('Submitted data=', nameTxt, passwordTxt, countryTxt)
 
         const nameErrorMessage = getNameError(nameTxt)
+        const passwordErrorMessage = getNameError(passwordTxt)
 
         if (nameErrorMessage) {
-            const nameErrorDisplay = document.getElementById('nameErrorDisplay');
             nameErrorDisplay.textContent = nameErrorMessage;
+        }
+        if (passwordErrorMessage) {
+            passwordErrorDisplay.textContent = passwordErrorMessage;
         }
     }
 })()
